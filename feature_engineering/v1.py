bd2493f03943d4a4c545773e5c4f8c6d634cdff3
Naoki Nakamichi <ronnao0516@gmail.com>
2022-03-26 14:58:02 +0900

    hoge

---
+++ feature_engineering/v1.py
@@ -0,0 +1,49 @@
+#%%
+
+import numpy as np
+import pandas as pd
+
+# 使用している特徴量
+# 階級　全国勝率　全国2連帯　当地勝率　当地２連帯率　モーター２連帯　ボート２連帯　展示スタートタイム　展示タイム　展示コース
+# それぞれ６艇の偏差値を算出
+
+usecols = [f'{k}_{i}' for k in ('class', 'glob_win', 'glob_in2',
+                                'loc_win', 'loc_in2', 'moter_in2', 'boat_in2', 'EST','ESC' )
+            for i in range(1, 7)]
+usecols += [f'ET_{i}_y'
+            for i in range(1, 7)]
+hoge = pd.read_csv("../dataset/all_data_2021.csv")
+
+tmp = hoge[usecols]
+
+def rank_mapper(x):
+    if 'A1' in x:
+        y = 4
+    elif 'A2' in x:
+        y = 3
+    elif 'B1' in x:
+        y = 2
+    else:
+        y = 1
+    return y
+
+for i in range(1,7):
+    col_name_class = f"class_{i}"
+    tmp.loc[:, col_name_class] = tmp.loc[:, col_name_class].map(rank_mapper)
+class_col = [s for s in tmp.columns if "class" in s]
+def hensachi(x):
+    if x.std() == 0:
+        return np.full(6,50.0)
+
+    hensa = (x - x.mean())*10/x.std()+50
+
+    return hensa
+tmp.loc[:, class_col] = tmp.loc[:,class_col].apply(hensachi,axis=1)
+
+hensa_col = ['glob_win', 'glob_in2','loc_win', 'loc_in2', 'moter_in2', 'boat_in2', 'EST', "ET","ESC"]
+
+for i in hensa_col:
+    class_col = [s for s in tmp.columns if i in s]
+    tmp.loc[: , class_col] = tmp.loc[:, class_col].apply(hensachi,axis=1)
+
+tmp.to_csv("../dataset/v1_features/v1.csv")
---
+++ raw_racelist_data_create.py
@@ -0,0 +1,24 @@
+import data_get
+import time
+
+import race_before_data
+
+
+years = [2020,2021,2022]
+months = [i for i in range(1,13)]
+days = [i for i in range(1,32)]
+
+for year_i in years:
+    for month_i in months:
+        for day_i in days:
+            time.sleep(5)
+            try:
+
+                date = f"{year_i}-{month_i}-{day_i}"
+                data_get.download_file('results', date)
+                df = data_get.get_results(date)
+                df.to_csv(f"outputs/{date}_result.csv", encoding='utf_8_sig')
+                print(f"success {date}",flush=True)
+
+            except:
+                print("error",flush=True)
